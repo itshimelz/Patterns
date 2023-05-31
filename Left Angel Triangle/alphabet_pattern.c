44bd3e39d8c51b142e1da1631825468f643bb598
itshimelz <himelhasan1215@gmail.com>
2023-06-01 02:29:46 +0600

    Added left angle triangle patterns.

--- Left Angel Triangle/alphabet_pattern.c
+++ Left Angel Triangle/alphabet_pattern.c
@@ -3,25 +3,19 @@
 int main()
 {
     int n, row, col;
-
     printf("Enter N = ");
     scanf("%d", &n);
 
     for (row = 1; row <= n; row++)
     {
-        for (col = 1; col <= row; col++)
+        for (col = 1; col <= n - row; col++)
         {
-            printf(" *");
+            printf(" ");
         }
 
-        printf("\n");
-    }
-
-    for (row = n - 1; row >= 1; row--)
-    {
         for (col = 1; col <= row; col++)
         {
-            printf(" *");
+            printf("%c", col + 64);
         }
 
         printf("\n");
--- Left Angel Triangle/number_pattern.c
+++ Left Angel Triangle/number_pattern.c
@@ -3,25 +3,21 @@
 int main()
 {
     int n, row, col;
-
     printf("Enter N = ");
     scanf("%d", &n);
 
     for (row = 1; row <= n; row++)
     {
-        for (col = 1; col <= row; col++)
+        // Print space
+        for (col = 1; col <= n - row; col++)
         {
-            printf(" *");
+            printf(" ");
         }
 
-        printf("\n");
-    }
-
-    for (row = n - 1; row >= 1; row--)
-    {
+        // Print number
         for (col = 1; col <= row; col++)
         {
-            printf(" *");
+            printf("%d ", col);
         }
 
         printf("\n");
